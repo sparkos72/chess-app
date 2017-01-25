759ffb86c992e606c79453314399ff7428e1ae4a
Mark Wingfield <me@mark-wingfield.co.uk>
2017-01-25 09:05:24 +0000

    place pieces

--- js/init.js
+++ js/init.js
@@ -8,7 +8,7 @@ function initBoard()
 	initBoardLayout();
 
 	var piecesData 								= getPiecePostitions();
-	positionPieces();
+	setPiecePositions(piecesData);
 }
 
 
@@ -48,45 +48,62 @@ function setBoardLayout()
 function getPiecePostitions(piecesData)
 {
 	var white 									= {
-		'p1'									: 'b1',
-		'p2'									: 'b2',
-		'p3'									: 'b3',
-		'p4'									: 'b4',
-		'p5'									: 'b5',
-		'p6'									: 'b6',
-		'p7'									: 'b7',
-		'p8'									: 'b8',
-		'qr'									: 'a1',
-		'kr'									: 'a8',
-		'qk'									: 'a2',
-		'kk'									: 'a7',
-		'qb'									: 'a3',
-		'kb'									: 'a6',
-		'q'										: 'a4',
-		'k'										: 'a5'
+		'p1'									: {'col':'b', 'row': 'sq-1'},
+		'p2'									: {'col':'b', 'row': 'sq-2'},
+		'p3'									: {'col':'b', 'row': 'sq-3'},
+		'p4'									: {'col':'b', 'row': 'sq-4'},
+		'p5'									: {'col':'b', 'row': 'sq-5'},
+		'p6'									: {'col':'b', 'row': 'sq-6'},
+		'p7'									: {'col':'b', 'row': 'sq-7'},
+		'p8'									: {'col':'b', 'row': 'sq-8'},
+		'qr'									: {'col':'a', 'row': 'sq-1'},
+		'kr'									: {'col':'a', 'row': 'sq-8'},
+		'qk'									: {'col':'a', 'row': 'sq-2'},
+		'kk'									: {'col':'a', 'row': 'sq-7'},
+		'qb'									: {'col':'a', 'row': 'sq-3'},
+		'kb'									: {'col':'a', 'row': 'sq-6'},
+		'q'										: {'col':'a', 'row': 'sq-4'},
+		'k'										: {'col':'a', 'row': 'sq-5'}
 	};
 	var black 									= {
-		'p1'									: 'b1',
-		'p2'									: 'b2',
-		'p3'									: 'b3',
-		'p4'									: 'b4',
-		'p5'									: 'b5',
-		'p6'									: 'b6',
-		'p7'									: 'b7',
-		'p8'									: 'b8',
-		'qr'									: 'a1',
-		'kr'									: 'a8',
-		'qk'									: 'a2',
-		'kk'									: 'a7',
-		'qb'									: 'a3',
-		'kb'									: 'a6',
-		'q'										: 'a4',
-		'k'										: 'a5'
+		'p1'									: {'col':'g', 'row': 'sq-1'},
+		'p2'									: {'col':'g', 'row': 'sq-2'},
+		'p3'									: {'col':'g', 'row': 'sq-3'},
+		'p4'									: {'col':'g', 'row': 'sq-4'},
+		'p5'									: {'col':'g', 'row': 'sq-5'},
+		'p6'									: {'col':'g', 'row': 'sq-6'},
+		'p7'									: {'col':'g', 'row': 'sq-7'},
+		'p8'									: {'col':'g', 'row': 'sq-8'},
+		'qr'									: {'col':'h', 'row': 'sq-1'},
+		'kr'									: {'col':'h', 'row': 'sq-8'},
+		'qk'									: {'col':'h', 'row': 'sq-2'},
+		'kk'									: {'col':'h', 'row': 'sq-7'},
+		'qb'									: {'col':'h', 'row': 'sq-3'},
+		'kb'									: {'col':'h', 'row': 'sq-6'},
+		'q'										: {'col':'h', 'row': 'sq-4'},
+		'k'										: {'col':'h', 'row': 'sq-5'}
 	};
 	return {'white': white, 'black': black};
 }
 
-function setPiecePostitions(piecesData)
+function setPiecePositions(piecesData)
 {
+	// set whites
+	var white									= piecesData.white;
+	$.each(white, function(piece, postition)
+	{
+		setPiecePosition(piece, postition, 'white')
+	});
 
+	// set blacks
+	var black									= piecesData.black;
+	$.each(black, function(piece, postition)
+	{
+		setPiecePosition(piece, postition, 'black')
+	});
+}
+
+function setPiecePosition(piece, postition, colour)
+{
+	$('#' + postition.col + ' .' + postition.row ).html(piece);
 }
